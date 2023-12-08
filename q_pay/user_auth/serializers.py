6c5931339369e583821c74bd9fd43a06f66a53c2
sergey_sobolenko <sergasobolenko@gmail.com>
2023-12-08 02:22:47 +0200

    added 2fa authentication based on TOTP.

---
+++ q_pay/user_auth/serializers.py
@@ -0,0 +1,79 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
+from rest_framework import serializers, exceptions
+from rest_framework_simplejwt.tokens import RefreshToken
+import pyotp
+
+from main.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    user_type = serializers.IntegerField()
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "password", "user_type")
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def create(self, validated_data: dict):
+        email = validated_data.get("email")
+        password = validated_data.get("password")
+        user_type = validated_data.get("user_type")
+        otp_base32 = pyotp.random_base32()
+        user_info = {
+            "email": email,
+            "password": password,
+            "user_type": user_type,
+            "otp_base32": otp_base32,
+        }
+        print("REG PASSWORD:", password, make_password(password))
+        user = User.objects.create(**user_info)
+        user.save()
+
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, attrs: dict):
+        user = authenticate(
+            request=self.context.get("request"),
+            email=attrs.get("email").lower().strip(),
+            password=attrs.get("password")
+        )
+        if not user:
+            raise exceptions.AuthenticationFailed("Invalid login credentials.")
+        attrs["user"] = user
+        return super().validate(attrs)
+
+    def create(self, validated_data: dict):
+        user = validated_data.get("user")
+        return user
+
+
+class UserVerifyOTPSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    otp = serializers.CharField()
+
+    def validate(self, attrs: dict):
+        user = User.objects.filter(pk=attrs.get("user_id")).first()
+        if not user:
+            raise exceptions.AuthenticationFailed("User with this ID does not exist.")
+
+        is_otp_valid = pyotp.TOTP(user.otp_base32).verify(attrs.get("otp"))
+        if not is_otp_valid:
+            raise exceptions.AuthenticationFailed("Invalid OTP.")
+        attrs["user"] = user
+        return super().validate(attrs)
+
+    def create(self, validated_data: dict):
+        user = validated_data.get("user")
+        refresh = RefreshToken.for_user(user)
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
---
+++ q_pay/user_auth/service.py
@@ -0,0 +1,7 @@
+def get_user_type_by_invite_code(invite_code: str) -> int | None:
+    """
+    Get user type from redis using invite code
+    """
+    # TODO: implement request to redis
+
+    return 1
