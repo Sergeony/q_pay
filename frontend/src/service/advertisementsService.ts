2292fcecdc53f069baa586754152bb6f1f479939
sergey_sobolenko <sergasobolenko@gmail.com>
2024-02-01 04:57:40 +0200

    added auto updating ads after any mutations

--- frontend/src/service/advertisementsService.ts
+++ frontend/src/service/advertisementsService.ts
@@ -13,15 +13,17 @@ import {BaseQueryArg} from "@reduxjs/toolkit/dist/query/baseQueryTypes";
 export const advertisementsApi = createApi({
   reducerPath: 'advertisementsApi',
   baseQuery,
+  tagTypes: ["Advertisement"],
   endpoints: (builder) => ({
     fetchAdvertisements: builder.query<advertisementsProps[], void>({
       query: () => ({
         url: 'api/v1/trader/advertisements/'
       }),
+      providesTags: ["Advertisement"],
       onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
         try {
           const { data } = await queryFulfilled;
-          dispatch(setAdvertisements( data ));
+          dispatch(setAdvertisements(data));
         } catch (error) {
           // Обработка ошибок
         }
@@ -33,12 +35,14 @@ export const advertisementsApi = createApi({
         method: 'POST',
         body
       }),
+      invalidatesTags: ["Advertisement"]
     }),
     deleteAdvertisement: builder.mutation<string, number>({
       query: (id) => ({
         url: `api/v1/trader/advertisements/${id}/`,
         method: 'DELETE',
       }),
+      invalidatesTags: ["Advertisement"],
     }),
     toggleAdvertisementActivity: builder.mutation<advertisementsProps, toggleAdvertisementActivityProps>({
       query: ({id, is_activated}) => ({
@@ -46,6 +50,7 @@ export const advertisementsApi = createApi({
         method: 'PATCH',
         body: {is_activated}
       }),
+      invalidatesTags: ["Advertisement"],
     }),
   }),
 })
