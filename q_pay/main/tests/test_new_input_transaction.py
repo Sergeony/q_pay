1cf6d887a3761bce59bbfe2950864b750ecef437
sergey_sobolenko <sergasobolenko@gmail.com>
2024-02-09 22:00:39 +0200

    updated create transaction test

--- q_pay/main/tests/test_new_input_transaction.py
+++ q_pay/main/tests/test_new_input_transaction.py
@@ -1,38 +1,55 @@
-from main.models import InputTransaction, User, Requisites
+from main.models import InputTransaction, User, Requisites, OutputTransaction
 from channels.layers import get_channel_layer
-from main.serializers import InputTransactionSerializer
+from main.serializers import InputTransactionSerializer, OutputTransactionSerializer
 from asgiref.sync import async_to_sync
 
-trader = User.objects.get(id=19)
+trader_id = 19
+trader = User.objects.get(id=trader_id)
 merchant = User.objects.get(id=3)
 requisites = Requisites.objects.get(id=11)
 
-new_transaction = InputTransaction.objects.create(
-    trader=trader,
-    merchant=merchant,
-    requisites=requisites,
-    status=InputTransaction.Status.PENDING_CLIENT_CONFIRMATION,
-    claimed_amount=100.00,
-    trader_usdt_rate=1.01,
-    exchange_usdt_rate=1.00,
-    automation_used=False
-)
-
-
 channel_layer = get_channel_layer()
-group_name = 'user_19'
-
-message = {
-    'type': 'send_new_transaction_alert',
-    'transaction_data': InputTransactionSerializer(new_transaction).data
-}
+group_name = f'user_{trader_id}'
 
 
-def perform():
-    async_to_sync(channel_layer.group_send)(
-        group_name,
-        message
-    )
+def create_input_transaction():
+	new_transaction = InputTransaction.objects.create(
+		trader=trader,
+		merchant=merchant,
+		requisites=requisites,
+		status=InputTransaction.Status.PENDING_CLIENT_CONFIRMATION,
+		claimed_amount=100.00,
+		trader_usdt_rate=1.01,
+		exchange_usdt_rate=1.00,
+		automation_used=False
+	)
 
-# from main.tests.test_new_input_transaction import perform
+	message = {
+		'type': 'send_new_transaction_alert',
+		'transaction_data': InputTransactionSerializer(new_transaction).data
+	}
 
+	async_to_sync(channel_layer.group_send)(group_name, message)
+
+
+def create_output_transaction():
+	new_transaction = OutputTransaction.objects.create(
+		trader=trader,
+		merchant=merchant,
+		requisites=requisites,
+		status=InputTransaction.Status.PENDING_CLIENT_CONFIRMATION,
+		claimed_amount=100.00,
+		trader_usdt_rate=1.01,
+		exchange_usdt_rate=1.00,
+		automation_used=False
+	)
+
+	message = {
+		'type': 'send_new_transaction_alert',
+		'transaction_data': OutputTransactionSerializer(new_transaction).data
+	}
+
+	async_to_sync(channel_layer.group_send)(group_name, message)
+
+
+# from main.tests.test_new_input_transaction import create_output_transaction, create_input_transaction
