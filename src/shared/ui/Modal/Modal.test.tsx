71602c59cabb26db4a7217512118edd6e81db926
sergey_sobolenko <sergasobolenko@gmail.com>
2024-03-06 05:10:27 +0200

    updated tests for modal

--- src/shared/ui/Modal/Modal.test.tsx
+++ src/shared/ui/Modal/Modal.test.tsx
@@ -5,17 +5,17 @@ describe("Modal", () => {
     const children = (<div />);
 
     test("Test render", () => {
-        render(<Modal isOpen>{children}</Modal>);
+        render(<div id="app"><Modal isLazy isOpen>{children}</Modal></div>);
         expect(screen.getByTestId("modal")).toBeInTheDocument();
     });
 
     test("Test render close button", () => {
-        render(<Modal isOpen>{children}</Modal>);
+        render(<div id="app"><Modal isLazy isOpen>{children}</Modal></div>);
         expect(screen.queryByTestId("close-button")).toBeInTheDocument();
     });
 
     test("Test don't render close button", () => {
-        render(<Modal isOpen hasCloseBtn={false}>{children}</Modal>);
+        render(<div id="app"><Modal isLazy isOpen hasCloseBtn={false}>{children}</Modal></div>);
         expect(screen.queryByTestId("close-button")).not.toBeInTheDocument();
     });
 });
